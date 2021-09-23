4eb57bf765747a7cc21eddb7ebdecf3286ba8626
Adrian Pop <Adrian.Pop@p4b.ro>
2021-09-23 13:40:57 +0300

    finished

--- index.js
+++ index.js
@@ -5,13 +5,23 @@
  * If the previous term is odd, the next term is 3 times the previous term plus 1
  * The conjecture is that no matter what value of n, the sequence will always reach 1.
  */
-
+function getRandomInteger(min, max) {
+    const integerMin = Math.ceil(min);
+    const integerMax = Math.floor(max);
+    return Math.floor(Math.random() * (integerMax - integerMin + 1)) + integerMin;
+  };
 function stepCounter(n) {
-    var counter = 0;
+    let counter = 0;
+    let listOfNumbers = []
 
     while (n > 1) {
+        listOfNumbers.push(n);
         n = n % 2 ? 3 * n + 1 : n / 2;
         counter++;
     }
-    return console.log(counter);
+    console.table(listOfNumbers);
+    console.log(`number has been reached in ${counter} steps`);
+    return
 }
+
+stepCounter(getRandomInteger(1, 100000000000))
